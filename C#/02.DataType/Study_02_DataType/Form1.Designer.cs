9139c5487e7b52e1745e5564fd023aebf40bfd65
Chan ho Moon <minsonyo@naver.com>
2022-02-27 19:35:07 +0900

    02. DataType

---
+++ C#/02.DataType/Study_02_DataType/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Study_02_DataType
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputData = new System.Windows.Forms.TextBox();
+            this.autobtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblShort = new System.Windows.Forms.Label();
+            this.lblInt = new System.Windows.Forms.Label();
+            this.lblDouble = new System.Windows.Forms.Label();
+            this.lblException = new System.Windows.Forms.Label();
+            this.btnShort = new System.Windows.Forms.Button();
+            this.btnInt = new System.Windows.Forms.Button();
+            this.btnDouble = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inputData
+            // 
+            this.inputData.Location = new System.Drawing.Point(24, 56);
+            this.inputData.Name = "inputData";
+            this.inputData.Size = new System.Drawing.Size(667, 21);
+            this.inputData.TabIndex = 0;
+            // 
+            // autobtn
+            // 
+            this.autobtn.Location = new System.Drawing.Point(707, 56);
+            this.autobtn.Name = "autobtn";
+            this.autobtn.Size = new System.Drawing.Size(81, 23);
+            this.autobtn.TabIndex = 1;
+            this.autobtn.Text = "알아서 변환";
+            this.autobtn.UseVisualStyleBackColor = true;
+            this.autobtn.Click += new System.EventHandler(this.autobtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Short";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Int";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 184);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Double";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(42, 216);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 12);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Exception";
+            // 
+            // lblShort
+            // 
+            this.lblShort.AutoSize = true;
+            this.lblShort.Location = new System.Drawing.Point(144, 127);
+            this.lblShort.Name = "lblShort";
+            this.lblShort.Size = new System.Drawing.Size(0, 12);
+            this.lblShort.TabIndex = 6;
+            // 
+            // lblInt
+            // 
+            this.lblInt.AutoSize = true;
+            this.lblInt.Location = new System.Drawing.Point(144, 157);
+            this.lblInt.Name = "lblInt";
+            this.lblInt.Size = new System.Drawing.Size(0, 12);
+            this.lblInt.TabIndex = 7;
+            // 
+            // lblDouble
+            // 
+            this.lblDouble.AutoSize = true;
+            this.lblDouble.Location = new System.Drawing.Point(144, 184);
+            this.lblDouble.Name = "lblDouble";
+            this.lblDouble.Size = new System.Drawing.Size(0, 12);
+            this.lblDouble.TabIndex = 8;
+            // 
+            // lblException
+            // 
+            this.lblException.AutoSize = true;
+            this.lblException.Location = new System.Drawing.Point(144, 216);
+            this.lblException.Name = "lblException";
+            this.lblException.Size = new System.Drawing.Size(0, 12);
+            this.lblException.TabIndex = 9;
+            // 
+            // btnShort
+            // 
+            this.btnShort.Location = new System.Drawing.Point(457, 127);
+            this.btnShort.Name = "btnShort";
+            this.btnShort.Size = new System.Drawing.Size(89, 23);
+            this.btnShort.TabIndex = 10;
+            this.btnShort.Text = "Short 변환";
+            this.btnShort.UseVisualStyleBackColor = true;
+            this.btnShort.Click += new System.EventHandler(this.btnShort_Click);
+            // 
+            // btnInt
+            // 
+            this.btnInt.Location = new System.Drawing.Point(457, 157);
+            this.btnInt.Name = "btnInt";
+            this.btnInt.Size = new System.Drawing.Size(89, 23);
+            this.btnInt.TabIndex = 11;
+            this.btnInt.Text = "Int 변환";
+            this.btnInt.UseVisualStyleBackColor = true;
+            this.btnInt.Click += new System.EventHandler(this.btnInt_Click);
+            // 
+            // btnDouble
+            // 
+            this.btnDouble.Location = new System.Drawing.Point(457, 186);
+            this.btnDouble.Name = "btnDouble";
+            this.btnDouble.Size = new System.Drawing.Size(89, 23);
+            this.btnDouble.TabIndex = 12;
+            this.btnDouble.Text = "Double 변환";
+            this.btnDouble.UseVisualStyleBackColor = true;
+            this.btnDouble.Click += new System.EventHandler(this.btnDouble_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(829, 325);
+            this.Controls.Add(this.btnDouble);
+            this.Controls.Add(this.btnInt);
+            this.Controls.Add(this.btnShort);
+            this.Controls.Add(this.lblException);
+            this.Controls.Add(this.lblDouble);
+            this.Controls.Add(this.lblInt);
+            this.Controls.Add(this.lblShort);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.autobtn);
+            this.Controls.Add(this.inputData);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputData;
+        private System.Windows.Forms.Button autobtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblShort;
+        private System.Windows.Forms.Label lblInt;
+        private System.Windows.Forms.Label lblDouble;
+        private System.Windows.Forms.Label lblException;
+        private System.Windows.Forms.Button btnShort;
+        private System.Windows.Forms.Button btnInt;
+        private System.Windows.Forms.Button btnDouble;
+    }
+}
+
---
+++ C#/02.DataType/Study_02_DataType/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Study_02_DataType
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShort_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                short sNumber = short.Parse(inputData.Text);
+
+                lblShort.Text = sNumber.ToString();
+                lblException.Text = "";
+            }
+            catch (Exception ex)
+            {
+                lblException.Text = ex.ToString();
+            }
+        }
+
+        private void btnInt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int iNumber = int.Parse(inputData.Text);
+
+                lblInt.Text = iNumber.ToString();
+                lblException.Text = "";
+            }
+            catch (Exception ex)
+            {
+                lblException.Text = ex.ToString();
+            }
+        }
+
+        private void btnDouble_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double dNumber = double.Parse(inputData.Text);
+
+                lblDouble.Text = dNumber.ToString();
+                lblException.Text = "";
+            }
+            catch (Exception ex)
+            {
+                lblException.Text = ex.ToString();
+            }
+        }
+
+        private void autobtn_Click(object sender, EventArgs e)
+        {
+            short sNumber = 0;
+            int iNumber = 0;
+            double dNumber = 0;
+
+           if (short.TryParse(inputData.Text, out sNumber))
+           {
+                lblShort.Text = inputData.Text.ToString();
+           }
+           else if (int.TryParse(inputData.Text, out iNumber))
+            {
+                lblInt.Text = inputData.Text.ToString();
+            }
+            else if (double.TryParse(inputData.Text, out dNumber))
+            {
+                lblDouble.Text = inputData.Text.ToString();
+            }
+            else
+            {
+                lblException.Text = "변환이 불가능합니다.";
+            }
+        }
+    }
+}
