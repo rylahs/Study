166abd9f54af41e314147d545556d6ef5d1d15c3
Chan ho Moon <minsonyo@naver.com>
2022-02-27 23:01:46 +0900

    0x12. Math

---
+++ CPP,CodingTest/Baaarking/0x12/15894.cpp
@@ -0,0 +1,23 @@
+// 0x12. Math
+// Written by : Rylah
+// Date : 2022.02.24
+// 15894. 수학은 체육과목입니다.
+// https://www.acmicpc.net/problem/15894
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	long long n;
+	cin >> n;
+
+	long long result = 4 * n;
+
+	cout << result << "\n";
+
+	return 0;
+}
