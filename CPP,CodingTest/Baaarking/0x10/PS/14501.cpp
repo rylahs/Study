97b0590169c3ee3bc9302c9ea0131b41c4ad077b
Chan ho Moon <minsonyo@naver.com>
2022-03-01 18:29:17 +0900

    0x10. Dynamic Programming

--- CPP,CodingTest/Baaarking/0x10/PS/14501.cpp
+++ CPP,CodingTest/Baaarking/0x10/PS/14501.cpp
@@ -1,16 +1,15 @@
-// 0x10. 다이나믹 프로그래밍
+// 0xFD. Practice
 // Written by : Rylah
-// Date : 2022.02.13
+// Date : 2022.02.18
 // 14501. 퇴사
-// https://www.acmicpc.net/problem/14501
-// https://www.acmicpc.net/source/38987128
+// 
 
 #include <bits/stdc++.h>
 using namespace std;
 
-int t[25];
-int p[25];
-int dp[25];
+int t[17];
+int p[17];
+int dp[17];
 int main(void)
 {
 	ios::sync_with_stdio(false);
@@ -21,7 +20,7 @@ int main(void)
 	for (int i = 1; i <= n; i++)
 		cin >> t[i] >> p[i];
 
-	for (int i = n; i >= 1; i--)
+	for (int i = n; i > 0; i--)
 	{
 		if (t[i] + i > n + 1)
 			dp[i] = dp[i + 1];
@@ -29,7 +28,7 @@ int main(void)
 			dp[i] = max(dp[i + 1], dp[t[i] + i] + p[i]);
 	}
 
-	cout << dp[1] << "\n";
+	cout << *max_element(dp + 1, dp + n + 1) << "\n";
 
 	return 0;
 }
