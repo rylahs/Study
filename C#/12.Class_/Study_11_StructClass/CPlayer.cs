6776c04d63963de14f2d01bb014beeef0fe17155
Chan ho Moon <minsonyo@naver.com>
2022-04-06 14:38:00 +0900

    c# study

---
+++ C#/12.Class_/Study_11_StructClass/CPlayer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study_12_StructClass
+{
+    class CPlayer
+    {
+        int iTest = 0;
+        public CPlayer()
+        {
+            iTest = 1;
+        }
+        ~CPlayer()
+        {
+            iTest = 0;
+        }
+        //public int iCount = 0; // 몇 회 진행 중
+
+        //public int iSun = 0; // 해에 대한 값
+        //public int iMoon = 0; // 달에 대한 값
+        //public int iStar = 0; // 별에 대한 값
+
+        public int iCardSum = 0; // 해, 달, 별을 더한 값
+
+        public int CardSum(int iSun, int iMoon, int iStar)
+        {
+            return iSun + iMoon + iStar;
+        }
+        // 결과를 String으로 변환 ( 화면에 보여주기 위해 사용
+
+        public string ResultText(int iCount, int iSun, int iMoon, int iStar, int iCardSum)
+        {
+            return string.Format($"{iCount}회 해 : {iSun} , 달 : {iMoon}, 별 : {iStar} => 합계는 {iCardSum} 입니다.");
+        }
+
+        public string PlayerPair(int iCount, int iP1CardSum, int iP2CardSum)
+        {
+            int iCheck = iP1CardSum - iP2CardSum;
+            
+            if (iCheck > 0)
+            {
+                return string.Format($"{iCount}회차 : Player 1이 {iCheck} 만큼 더 큽니다.");
+            }
+            else if (iCheck < 0)
+            {
+                return string.Format($"{iCount}회차 : Player 2가 {Math.Abs(iCheck)} 만큼 더 큽니다.");
+            }
+            else
+                return string.Format($"{iCount}회차 : Player 1과 Player 2의 값이 같습니다.");
+        }
+
+        public string PlayerResult(int iP1CardSum, int iP2CardSum)
+        {
+           string strReturn = string.Empty;
+
+            if (iP1CardSum > iP2CardSum)
+            {
+                strReturn = "Player 1이 이겼습니다.";
+            }
+            else if (iP2CardSum > iP1CardSum)
+            {
+                strReturn = "Player 2가 이겼습니다.";
+            }
+            else
+                strReturn = "비겼습니다.";
+
+            return strReturn;
+        }
+    }
+}
---
+++ C#/12.Class_/Study_11_StructClass/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Study_12_StructClass;
+namespace Study_11_StructClass
+{
+    public partial class Form1 : Form
+    {
+        struct structPlayer
+        {
+            public int iCount; // 몇 회 진행 중
+
+            public int iSun; // 해에 대한 값
+            public int iMoon; // 달에 대한 값
+            public int iStar; // 별에 대한 값
+
+            public int iCardSum; // 해, 달, 별을 더한 값
+
+            //public int CardSum(int iSum, int iMoon, int iStar)
+            //{
+            //    return iSum + iMoon + iStar;
+            //}
+            //// 결과를 String으로 변환 ( 화면에 보여주기 위해 사용
+
+            //public string ResultText()
+            //{
+            //    return string.Format($"{iCount}회 해 : {iSun} , 달 : {iMoon}, 별 : {iStar} => 합계는 {iCardSum} 입니다.");
+            //}
+        }
+
+
+        structPlayer stPlayer1;
+        structPlayer stPlayer2;
+
+        Random rand = new Random();
+
+        
+        // Study_12_StructClass.CPlayer clPlayer2 = new CPlayer();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pboxSun_Click(object sender, EventArgs e)
+        {
+            int iNumber = rand.Next(1, 21);
+            if (rdoPlayer1.Checked)
+            {
+                stPlayer1.iSun = iNumber;
+            }
+            else
+            {
+                stPlayer2.iSun = iNumber;
+            }
+            Result();
+        }
+
+        private void pboxMoon_Click(object sender, EventArgs e)
+        {
+            int iNumber = rand.Next(1, 21);
+            if (rdoPlayer1.Checked)
+            {
+                stPlayer1.iMoon = iNumber;
+            }
+            else
+            {
+                stPlayer2.iMoon = iNumber;
+            }
+            Result();
+
+        }
+
+        private void pboxStar_Click(object sender, EventArgs e)
+        {
+            int iNumber = rand.Next(1, 21);
+            if (rdoPlayer1.Checked)
+            {
+                stPlayer1.iStar = iNumber;
+            }
+            else
+            {
+                stPlayer2.iStar = iNumber;
+            }
+            Result();
+
+        }
+
+        private void pboxNone_Click(object sender, EventArgs e)
+        {
+            // 아무것도 안하고 턴을 넘긴다. 
+            Result();
+
+        }
+
+        private void iCheckedChange()
+        {
+            if(rdoPlayer1.Checked)
+            {
+                rdoPlayer2.Checked = true;
+            }
+            else
+            {
+                rdoPlayer1.Checked = true; 
+            }
+        }
+
+        Study_12_StructClass.CPlayer clPlayer = new CPlayer();
+        private void Result()
+        {
+            string strResult = string.Empty;
+
+            if (rdoPlayer1.Checked)
+            {
+                stPlayer1.iCount++;
+                stPlayer1.iCardSum = clPlayer.CardSum(stPlayer1.iSun, stPlayer1.iMoon, stPlayer1.iStar);
+
+                strResult = clPlayer.ResultText(stPlayer1.iCount, stPlayer1.iSun, stPlayer1.iMoon, stPlayer1.iStar, stPlayer1.iCardSum);
+                lboxResult1.Items.Add(strResult);
+            }
+            
+            else
+            {
+                stPlayer2.iCount++;
+                stPlayer2.iCardSum = clPlayer.CardSum(stPlayer2.iSun, stPlayer2.iMoon, stPlayer2.iStar);
+
+                strResult = clPlayer.ResultText(stPlayer2.iCount, stPlayer2.iSun, stPlayer2.iMoon, stPlayer2.iStar, stPlayer2.iCardSum);
+                lboxResult2.Items.Add(strResult);
+            }
+            iCheckedChange();
+
+            if (stPlayer1.iCount == stPlayer2.iCount)
+            {
+                lboxNow.Items.Add(clPlayer.PlayerPair(stPlayer1.iCount, stPlayer1.iCardSum, stPlayer2.iCardSum));
+
+                if (stPlayer2.iCount > 5)
+                {
+                    lboxNow.Items.Add(clPlayer.PlayerResult(stPlayer1.iCardSum, stPlayer2.iCardSum));
+                }
+            }
+
+            //if (stPlayer1.iCount >= 5 && stPlayer2.iCount >= 5)
+            //{
+            //    if (clPlayer1.iCardSum > clPlayer2.iCardSum)
+            //        MessageBox.Show("Player1이 이겼습니다.");
+            //    else if (clPlayer1.iCardSum < clPlayer2.iCardSum)
+            //        MessageBox.Show("Player2가 이겼습니다.");
+            //    else
+            //        MessageBox.Show("비겼습니다.");
+            //}
+        }
+
+        //struct Car
+        //{
+        //    int wheel;
+        //    int cycle;
+        //    int window;
+        //    int door;
+        //    string name;
+        //}
+        //class cCar
+        //{
+        //    int wheel;
+        //    int cycle;
+        //    int window;
+        //    int door;
+        //    string name;
+        //}
+    }
+}
